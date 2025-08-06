1401102edaaf4f633240d89d6d03725490f250c2
navneetkang <navneetkang@gmail.com>
2025-08-06 19:26:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pickle
 import string
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download(...)
 ps = PorterStemmer()
 
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
